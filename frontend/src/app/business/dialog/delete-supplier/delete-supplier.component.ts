97551e0d051ed2dd182b2ffa14e54650090e32e7
Sergii Kozhushko <sergiikozhushko@gmail.com>
2023-08-07 14:52:05 +0200

    bad things

---
+++ frontend/src/app/business/dialog/delete-supplier/delete-supplier.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-supplier',
+  templateUrl: './delete-supplier.component.html',
+  styleUrls: ['./delete-supplier.component.css']
+})
+export class DeleteSupplierComponent {
+
+}
