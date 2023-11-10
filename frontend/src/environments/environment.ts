a70f98881c52bb6741b3bdf24d3333f813535bce
Sergii Kozhushko <sergiikozhushko@gmail.com>
2023-11-10 16:49:13 +0100

    front env

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,30 +3,30 @@
 // The list of file replacements can be found in `angular.json`.
 
 // global constants PROD
-// export const environment = {
-//   production: false,
-//   resourceServerURL: 'http://backend:8082', // root backend URL
-//   frontendURL: 'https://myshopapp.kyiv.ua:4200', // root URL frontend
-//   kcClientID: 'myshopapp-client', // KeyCloak client property
-//   kcBaseURL: 'https://myshopapp.kyiv.ua:8443/realms/myshop-realm/protocol/openid-connect',
-//   bffURI: 'https://myshopapp.kyiv.ua:8902/bff',
-//   redirectURI: 'https://myshopapp.kyiv.ua:4200',
-//   scopes: 'openid phone',
-//   devMode: true
-// };
-
-// global constants DEV
 export const environment = {
   production: false,
-  resourceServerURL: 'http://localhost:8082', // root backend URL
-  frontendURL: 'https://localhost:4200', // root URL frontend
+  resourceServerURL: 'http://backend:8082', // root backend URL
+  frontendURL: 'https://myshopapp.kyiv.ua:4200', // root URL frontend
   kcClientID: 'myshopapp-client', // KeyCloak client property
-  kcBaseURL: 'https://localhost:8443/realms/myshop-realm/protocol/openid-connect',
-  bffURI: 'https://localhost:8902/bff',
-  redirectURI: 'https://localhost:4200',
+  kcBaseURL: 'https://myshopapp.kyiv.ua:8443/realms/myshop-realm/protocol/openid-connect',
+  bffURI: 'https://myshopapp.kyiv.ua:8902/bff',
+  redirectURI: 'https://myshopapp.kyiv.ua:4200',
   scopes: 'openid phone',
   devMode: true
 };
+
+// global constants DEV
+// export const environment = {
+//   production: false,
+//   resourceServerURL: 'http://localhost:8082', // root backend URL
+//   frontendURL: 'https://localhost:4200', // root URL frontend
+//   kcClientID: 'myshopapp-client', // KeyCloak client property
+//   kcBaseURL: 'https://localhost:8443/realms/myshop-realm/protocol/openid-connect',
+//   bffURI: 'https://localhost:8902/bff',
+//   redirectURI: 'https://localhost:4200',
+//   scopes: 'openid phone',
+//   devMode: true
+// };
 
 
 
